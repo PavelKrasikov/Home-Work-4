2e35d580e69c3594efa7a9507805347d71c8d650
Pavel <kraspasha@mail.ru>
2023-03-30 13:38:34 +0300

    Final solution

---
+++ Кол-во чётных чисел в массиве.cs
@@ -0,0 +1,38 @@
+﻿//Задайте массив заполненный случайными положительными трёхзначными числами. 
+// Напишите программу, которая покажет количество чётных чисел в массиве [345, 897, 568, 234] -> 2
+
+void FillArray (int[] arr) //-----Создаём массив
+{
+    for (int ind = 0; ind < arr.Length; ind++)
+    {
+        arr[ind] = new Random().Next(100, 1000);
+    }
+}
+
+void PrintArray (int[] array) //------Печатаем массив
+{
+    Console.Write("[ ");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine("]");
+}
+
+int ShowDigit (int[] mass)
+{
+    int count = 0;
+    for (int i = 0; i < mass.Length; i++)
+    {
+        if (mass[i]%2 == 0)
+       {
+        count = count + 1;
+       }
+    }
+    return count;
+}
+
+int[] Arr = new int [8];
+FillArray (Arr);
+PrintArray (Arr);
+Console.WriteLine("Количество чётных чисел вмассиве " + ShowDigit (Arr));
